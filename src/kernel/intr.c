92e14a7a8348af4e09f7b566c0836a356ce80dda
Ziemas <ziemas@ziemas.se>
2026-06-12 16:57:16 +0200

    wip

--- src/kernel/intr.c
+++ src/kernel/intr.c
@@ -6,25 +6,31 @@ struct list_head {
 
 extern struct list_head D_8001B410;
 extern int D_8001B400;
-struct list_head *func_80006420(struct list_head *list);
+struct list_head* func_80006420(struct list_head* list);
+void func_800064D0(struct list_head*, struct list_head*);
+extern struct list_head D_8001B590[];
 
-
-struct list_head *func_800015E0()
+struct list_head* func_800015E0()
 {
-	struct list_head* i;
+    struct list_head* i;
 
-	i = func_80006420(&D_8001B410);
-	if (!i) {
-		return 0;
-	}
+    i = func_80006420(&D_8001B410);
+    if (!i) {
+        return 0;
+    }
 
-	D_8001B400++;
+    D_8001B400++;
 
-	return i;
+    return i;
 }
 
-INCLUDE_ASM("asm/kernel/nonmatchings/intr", func_80001628);
+void func_80001628(int arg0)
+{
+    func_800064D0(&D_8001B410, (arg0 * 0x18) + (void*)D_8001B590);
+    D_8001B400 -= 1;
+}
 
+// default intc handler
 INCLUDE_ASM("asm/kernel/nonmatchings/intr", func_80001670);
 
 INCLUDE_ASM("asm/kernel/nonmatchings/intr", func_800017D8);
