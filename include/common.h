2cfbe454304070b03f3a36ee679c5fdc5805e7ff
Ziemas <ziemas@ziemas.se>
2026-06-12 21:12:25 +0200

    wip

--- include/common.h
+++ include/common.h
@@ -3,49 +3,88 @@
 
 #include "include_asm.h"
 
-typedef unsigned char		u_char;
-typedef unsigned short		u_short;
-typedef unsigned int		u_int;
-typedef unsigned long		u_long;
+typedef unsigned char u_char;
+typedef unsigned short u_short;
+typedef unsigned int u_int;
+typedef unsigned long u_long;
 
-#define PIFREG ((volatile struct pif_reg *)0xbf803800)
+#define NULL ((void*)0)
 
-struct pif_reg {
-	/* 0x00 */ u_short board_id;
-	/* 0x02 */ u_short unk2;
-	/* 0x04 */ u_short rcv_state;
-	/* 0x06 */ u_short unk6;
-	/* 0x08 */ u_short cpr;
-	/* 0x0a */ u_short unkA;
-	/* 0x0c */ u_short com_send;
-	/* 0x0e */ u_short unkE;
-	/* 0x10 */ u_short fifo_stat;
-	/* 0x12 */ u_short unk12;
-	/* 0x14 */ u_short tx_count;
-	/* 0x16 */ u_short unk16;
-	/* 0x18 */ u_short rx_count;
-	/* 0x1a */ u_short unk1A;
-	/* 0x1c */ u_short unk1C;
-	/* 0x1e */ u_short unk1E;
-	/* 0x20 */ u_short unk20;
-	/* 0x22 */ u_short unk22;
-	/* 0x24 */ u_short unk24;
-	/* 0x26 */ u_short unk26;
-	/* 0x28 */ u_short unk28;
-	/* 0x2a */ u_short unk2A;
-	/* 0x2c */ u_short unk2C;
-	/* 0x2e */ u_short unk2E;
-	/* 0x30 */ u_short unk30;
-	/* 0x32 */ u_short unk32;
-	/* 0x34 */ u_short dmactrl;
-	/* 0x36 */ u_short unk36;
-	/* 0x38 */ u_short unk38;
-	/* 0x3a */ u_short unk3A;
-	/* 0x3c */ u_short dma_slice_count;
-	/* 0x3e */ u_short unk3E;
-	/* 0x40 */ u_int fifo;
+enum {
+    INTC_GS = 0,
+    INTC_SBUS = 1,
+    INTC_VBLANK_S = 2,
+    INTC_VBLANK_E = 3,
+    INTC_VIF0 = 4,
+    INTC_VIF1 = 5,
+    INTC_VU0 = 6,
+    INTC_VU1 = 7,
+    INTC_IPU = 8,
+    INTC_TIM0 = 9,
+    INTC_TIM1 = 10,
+    INTC_TIM2 = 11,
+    INTC_TIM3 = 12,
+    INTC_SFIFO = 13,
+    INTC_VU0WD = 14,
+	INTC_MAX,
 };
 
-int kprintf(const char *fmt, ...);
+enum {
+    DMAC_VIF0 = 0,
+    DMAC_VIF1 = 1,
+    DMAC_GIF = 2,
+    DMAC_FROM_IPU = 3,
+    DMAC_TO_IPU = 4,
+    DMAC_SIF0 = 5,
+    DMAC_SIF1 = 6,
+    DMAC_SIF2 = 7,
+    DMAC_FROM_SPR = 8,
+    DMAC_TO_SPR = 9,
+
+    DMAC_CIS = 13,
+    DMAC_MEIS = 14,
+    DMAC_BEIS = 15,
+	DMAC_MAX,
+};
+
+#define PIFREG ((volatile struct pif_reg*)0xbf803800)
+
+struct pif_reg {
+    /* 0x00 */ u_short board_id;
+    /* 0x02 */ u_short unk2;
+    /* 0x04 */ u_short rcv_state;
+    /* 0x06 */ u_short unk6;
+    /* 0x08 */ u_short cpr;
+    /* 0x0a */ u_short unkA;
+    /* 0x0c */ u_short com_send;
+    /* 0x0e */ u_short unkE;
+    /* 0x10 */ u_short fifo_stat;
+    /* 0x12 */ u_short unk12;
+    /* 0x14 */ u_short tx_count;
+    /* 0x16 */ u_short unk16;
+    /* 0x18 */ u_short rx_count;
+    /* 0x1a */ u_short unk1A;
+    /* 0x1c */ u_short unk1C;
+    /* 0x1e */ u_short unk1E;
+    /* 0x20 */ u_short unk20;
+    /* 0x22 */ u_short unk22;
+    /* 0x24 */ u_short unk24;
+    /* 0x26 */ u_short unk26;
+    /* 0x28 */ u_short unk28;
+    /* 0x2a */ u_short unk2A;
+    /* 0x2c */ u_short unk2C;
+    /* 0x2e */ u_short unk2E;
+    /* 0x30 */ u_short unk30;
+    /* 0x32 */ u_short unk32;
+    /* 0x34 */ u_short dmactrl;
+    /* 0x36 */ u_short unk36;
+    /* 0x38 */ u_short unk38;
+    /* 0x3a */ u_short unk3A;
+    /* 0x3c */ u_short dma_slice_count;
+    /* 0x3e */ u_short unk3E;
+    /* 0x40 */ u_int fifo;
+};
+
+int kprintf(const char* fmt, ...);
 
 #endif // COMMON_H_
--- src/kernel/intr.c
+++ src/kernel/intr.c
@@ -1,15 +1,52 @@
 #include "common.h"
 
 struct list_head {
-    struct list_head *next, *prev;
+    // TODO think about the order
+    struct list_head *prev, *next;
 };
 
+struct IntHandler {
+    /* 0x00 */ struct list_head l;
+    /* 0x08 */ void (*handler)();
+    /* 0x0c */ int unkC;
+    /* 0x10 */ void* arg;
+    /* 0x14 */ int state;
+};
+
+struct IntQueue {
+    struct list_head l;
+    int entries;
+};
+
+extern struct IntQueue D_8001B418[INTC_MAX];
+extern struct IntQueue D_8001B4D0[DMAC_MAX];
+
+extern struct IntHandler D_8001b590[129];
+
+// int handler pool list
 extern struct list_head D_8001B410;
+// number of used int handlers
 extern int D_8001B400;
+
+// list pop
 struct list_head* func_80006420(struct list_head* list);
+// list insert
 void func_800064D0(struct list_head*, struct list_head*);
+
+// backing array for ntc pool entries
 extern struct list_head D_8001B590[];
 
+// VSYNC flag ptr
+extern int* D_8001B404;
+// VSYNC CSR ptr
+extern u_long* D_8001B408;
+
+void func_80000DC0(int);
+int func_800029C0(void*, void*, ...);
+
+extern void* D_800174E4;
+
+// pop int handler pool
 struct list_head* func_800015E0()
 {
     struct list_head* i;
@@ -24,16 +61,93 @@ struct list_head* func_800015E0()
     return i;
 }
 
+// return to pool
 void func_80001628(int arg0)
 {
     func_800064D0(&D_8001B410, (arg0 * 0x18) + (void*)D_8001B590);
     D_8001B400 -= 1;
 }
 
-// default intc handler
-INCLUDE_ASM("asm/kernel/nonmatchings/intr", func_80001670);
+// INTC irq handler
+void func_80001670(int inum)
+{
+    struct IntHandler* hnd;
+    u_int gp, epc;
+    int i, res;
 
-INCLUDE_ASM("asm/kernel/nonmatchings/intr", func_800017D8);
+    if (inum == INTC_VBLANK_S && D_8001B404) {
+        *D_8001B404 = 1;
+        D_8001B404 = NULL;
+
+        if (D_8001B408) {
+            // read GS CSR
+            *D_8001B408 = *(volatile u_long*)0xb2001000;
+            D_8001B408 = NULL;
+        }
+    }
+
+    hnd = (struct IntHandler*)D_8001B418[inum].l.next;
+    for (i = 0; i < D_8001B418[inum].entries; i++) {
+        if (hnd->state == 2) {
+            __asm__ __volatile__("move %0, $gp \n" : "=r"(gp));
+            __asm__ __volatile__("move $gp, %0 \n" ::"r"(hnd->unkC));
+            __asm__ __volatile__("mfc0 %0, $14 \n" : "=r"(epc));
+
+            res = func_800029C0(D_800174E4, hnd->handler, inum, hnd->arg, epc);
+
+            if (res < 0) {
+                __asm__ __volatile__("move $gp, %0 \n" ::"r"(gp));
+                return;
+            }
+
+            __asm__ __volatile__("move $gp, %0 \n" ::"r"(gp));
+        } else if (hnd->state == 0) {
+            hnd->handler(inum);
+        }
+
+        hnd = (struct IntHandler*)hnd->l.next;
+    }
+
+    if (i == 0) {
+        kprintf("# INTC(%d) Handler does not exist.\n", inum);
+        func_80000DC0(1);
+    }
+}
+
+// DMAC irq handler
+void func_800017D8(int inum)
+{
+    struct IntHandler* hnd;
+    u_int gp, epc;
+    int i, res;
+
+    hnd = (struct IntHandler*)D_8001B4D0[inum].l.next;
+    for (i = 0; i < D_8001B4D0[inum].entries; i++) {
+        if (hnd->state == 2) {
+            __asm__ __volatile__("move %0, $gp \n" : "=r"(gp));
+            __asm__ __volatile__("move $gp, %0 \n" ::"r"(hnd->unkC));
+            __asm__ __volatile__("mfc0 %0, $14 \n" : "=r"(epc));
+
+            res = func_800029C0(D_800174E4, hnd->handler, inum, hnd->arg, epc);
+
+            if (res < 0) {
+                __asm__ __volatile__("move $gp, %0 \n" ::"r"(gp));
+                return;
+            }
+
+            __asm__ __volatile__("move $gp, %0 \n" ::"r"(gp));
+        } else if (hnd->state == 0) {
+            hnd->handler(inum);
+        }
+
+        hnd = (struct IntHandler*)hnd->l.next;
+    }
+
+    if (i == 0) {
+        kprintf("# DMAC(%d) Handler does not exist.\n", inum);
+        func_80000DC0(1);
+    }
+}
 
 INCLUDE_ASM("asm/kernel/nonmatchings/intr", func_800018F0);
 
